0c5ef5d233e8b01ba6a365563ccc193ff9ec3390
Samuel Adeyemi <samueladeyemi2006@gmail.com>
2022-04-23 08:28:14 +0100

    Revert "Merge pull request #3 from Samueljr-web/revert-2-products"
    This reverts commit 1b654322e23018af34874057bb8d43242d6a4014, reversing
    changes made to 55c3b12ceea0aceb141c5b776718f13cdac6d450.

---
+++ src/components/Product/product.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./product.css";
+
+const Product = () => {
+    return (
+        <>
+            <header>
+                <h2>Our Products</h2>
+            </header>
+            <section className="product__listing">
+                <div className="product__listing-item">
+                    <img
+                        src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg"
+                        alt=""
+                    />
+                </div>
+                <div className="product__listing-item">
+                    <img
+                        src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg"
+                        alt=""
+                    />
+                </div>
+                <div className="product__listing-item">
+                    <img
+                        src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg"
+                        alt=""
+                    />
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default Product;
