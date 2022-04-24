b1d922919ec296d26b77d8d26d81425696f3a37f
Odiri Teddie <odiriteddie.adamu@gmail.com>
2022-04-24 17:18:02 -0400

    worked on footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,67 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faFacebookF, faTwitter, faInstagram, faPinterestP} from '@fortawesome/free-brands-svg-icons';
+
 import "./Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
-      <div>
-        <div>
-          <h2>Links</h2>
-          <ul>
-            <li></li>
-          </ul>
+      <div className="container">
+        <div className="top-part">
+
+          <div className="row">
+            <h2>Logo</h2>
+            <p>Slogan Company</p>
+          </div>
+          
+          <div className="row">
+            <h2>Logo</h2>
+            <ul>
+              <li>Meal</li>
+              <li>Meal</li>
+              <li>Meal</li>
+            </ul>
+          </div>
+
+          <div className="row">
+            <h2>Logo</h2>
+            <ul>
+              <li>Meal</li>
+              <li>Meal</li>
+              <li>Meal</li>
+            </ul>
+          </div>
+
+          <div className="row">
+            <h2>Logo</h2>
+            <ul>
+              <li>Meal</li>
+              <li>Meal</li>
+              <li>Meal</li>
+            </ul>
+          </div>
+
+          <div className="row">
+            <h2>Logo</h2>
+            <ul>
+              <li>Meal</li>
+              <li>Meal</li>
+              <li>Meal</li>
+            </ul>
+          </div>
         </div>
+
+        <div className="lower-part">
+            <div className="footer-socials">
+                <div><FontAwesomeIcon icon= {faFacebookF} /></div>
+                <div><FontAwesomeIcon icon= {faTwitter} /></div>
+                <div><FontAwesomeIcon icon= {faInstagram} /></div>
+                <div><FontAwesomeIcon icon= {faPinterestP} /></div>
+            </div>
+            <small>©Copyright All rights reserved</small>
+        </div>
+
       </div>
     </div>
   );
