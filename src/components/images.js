cfdada993b0f68784ab9bb5399310cabde3ef051
Samuel Adeyemi <samueladeyemi2006@gmail.com>
2022-04-25 23:10:19 +0100

    added new images

--- src/components/images.js
+++ src/components/images.js
@@ -9,10 +9,23 @@ import aboutImg4 from "../assets/about-img-4.jpg";
 import aboutImg5 from "../assets/about-img-5.jpg";
 import aboutImg6 from "../assets/about-img-7.jpg";
 import aboutImg7 from "../assets/about-img-8.jpg";
-import dish from '../assets/dish.png'
-import fork from '../assets/fork.png'
+import dish from "../assets/dish.png";
+import fork from "../assets/fork.png";
 
-
-const images = [image1, image2, image3, image4, image5, aboutImg2, aboutImg3, aboutImg4, aboutImg5, dish, aboutImg6, aboutImg7, fork];
+const images = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  aboutImg2,
+  aboutImg3,
+  aboutImg4,
+  aboutImg5,
+  dish,
+  aboutImg6,
+  aboutImg7,
+  fork,
+];
 
 export default images;
